a63988b9748cced222f0a2c2b4bb168f281fd483
Kukyeon <rnrdus2000@naver.com>
2025-08-27 10:53:31 +0900

    매게 변수에 어노테이션으로 파라미터 값 받아오기

--- src/main/java/com/kkuk/formtest/MemberContrller.java
+++ src/main/java/com/kkuk/formtest/MemberContrller.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberContrller {
@@ -15,6 +16,10 @@ public class MemberContrller {
 	@RequestMapping(value = "/login")
 	public String login() {
 		return "login";
+	}
+	@RequestMapping(value = "/login2")
+	public String login2() {
+		return "login2";
 	}
 	
 	
@@ -35,4 +40,24 @@ public class MemberContrller {
 			return "loginFail";
 		}	
 	}
+	
+	@RequestMapping(value = "/loginOk2")
+	public String loginOk2(@RequestParam("mid")String mid,@RequestParam("mpw")String mpw, Model model, HttpSession session, HttpServletResponse response) {
+		
+//		String mid = request.getParameter("mid"); // 클라이언트가 입력한 아이디값 불러오기
+			//@RequestParam("mid")String mid
+//		String mpw = request.getParameter("mpw");	// 클라이언트가 입력한 비밀번호값 불러오기
+			//@RequestParam("mpw")String mpw
+		model.addAttribute("mid", mid);
+		
+		if(mid.equals("tiger") && mpw.equals("12345")) {
+			session.setAttribute("sessionId", mid); // 세션에 로그인한 아이디 올리기
+			
+			return "loginOk";
+		}else {
+			
+			return "loginFail";
+		}	
+	}
+	
 }
